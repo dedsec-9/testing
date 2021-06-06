c5aa89993b12e5fb87de3d8d1eaa193d60e890f3
dedsec-9 <77307684+dedsec-9@users.noreply.github.com>
2021-06-06 18:09:01 -0400

    Unshorten unclear abbreviation

---
+++ .github/workflows/matlab-actions/run-command/src/matlab.ts
@@ -0,0 +1,73 @@
+// Copyright 2020 The MathWorks, Inc.
+
+import { promises as fs } from "fs";
+import * as os from "os";
+import * as path from "path";
+import { v4 as uuid } from "uuid";
+import * as script from "./script";
+
+/**
+ * Helper interface to represent a MATLAB script.
+ */
+export interface HelperScript {
+    dir: string;
+    command: string;
+}
+
+/**
+ * Type of a function that executes a command on a runner and returns the error
+ * code.
+ */
+export type ExecFn = (command: string, args?: string[]) => Promise<number>;
+
+/**
+ * Generate a MATLAB script in the temporary directory that runs a command in
+ * the workspace.
+ *
+ * @param workspaceDir CI job workspace directory
+ * @param command MATLAB command to run
+ */
+export async function generateScript(workspaceDir: string, command: string): Promise<HelperScript> {
+    const scriptName = script.safeName(`command_${uuid()}`);
+
+    const temporaryDirectory = await fs.mkdtemp(path.join(os.tmpdir(), "run_matlab_command-"));
+
+    const scriptPath = path.join(temporaryDirectory, scriptName + ".m");
+    await fs.writeFile(scriptPath, script.cdAndCall(workspaceDir, command), {
+        encoding: "utf8",
+    });
+
+    return { dir: temporaryDirectory, command: scriptName };
+}
+
+/**
+ * Run a HelperScript in MATLAB.
+ *
+ * Create the HelperScript using `generateScript`.
+ *
+ * @param hs HelperScript pointing to the script containing the command
+ * @param platform Operating system of the runner (e.g., "win32" or "linux")
+ * @param fn ExecFn that will execute a command on the runner
+ */
+export async function runCommand(hs: HelperScript, platform: string, fn: ExecFn): Promise<void> {
+    const rmcPath = getRunMATLABCommandScriptPath(platform);
+    await fs.chmod(rmcPath, 0o777);
+
+    const rmcArg = script.cdAndCall(hs.dir, hs.command);
+
+    const exitCode = await fn(rmcPath, [rmcArg]);
+    if (exitCode !== 0) {
+        return Promise.reject(Error(`Exited with non-zero code ${exitCode}`));
+    }
+}
+
+/**
+ * Get the path of the script containing RunMATLABCommand for the host OS.
+ *
+ * @param platform Operating system of the runner (e.g., "win32" or "linux")
+ */
+export function getRunMATLABCommandScriptPath(platform: string): string {
+    const ext = platform === "win32" ? "bat" : "sh";
+    const rmcPath = path.join(__dirname, "bin", `run_matlab_command.${ext}`);
+    return rmcPath;
+}
